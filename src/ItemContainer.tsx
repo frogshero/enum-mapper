55dc56e7a5fab3bd96c5424fe82e4a890de820e6
hubery <guofu.wang@dianrong.com>
2019-12-10 15:55:49 +0800

    fc

--- src/ItemContainer.tsx
+++ src/ItemContainer.tsx
@@ -2,58 +2,53 @@ import React from 'react';
 import {ContainerParam, DragStartInfo, FromToColumn} from './EnumInfo'
 import ItemForDrag from './ItemForDrag'
 
-export default class ItemContaner extends React.Component<ContainerParam> {
-	allowDrop = (ev: React.DragEvent) => {
-        ev.preventDefault();
-    }
-    
-    dragStart = (e: React.DragEvent, fromIdx: number) => {
-        let dragStartInfo: DragStartInfo = {fromArrIdx: fromIdx, rowIdx: this.props.rowIdx, col: this.props.col};
+const ItemContaner = (props: ContainerParam) => {
+    let dragStart = (e: React.DragEvent, fromIdx: number) => {
+        let dragStartInfo: DragStartInfo = {fromArrIdx: fromIdx, rowIdx: props.rowIdx, col: props.col};
 		e.dataTransfer.setData("text/plain", JSON.stringify(dragStartInfo));
     }
 
-    containerClick = (e: React.MouseEvent) => {
-        if (this.props.col === FromToColumn.from) return;
-        this.props.containerClick(this.props.col, this.props.rowIdx);
+    let containerClick = (e: React.MouseEvent) => {
+        if (props.col === FromToColumn.from) return;
+        props.containerClick(props.col, props.rowIdx);
     }
 
-    containerDrop = (e: React.DragEvent) => {
+    let containerDrop = (e: React.DragEvent) => {
         //利用事件冒泡，只在container处理
         e.preventDefault();
 
         let data = e.dataTransfer.getData("text/plain");
         let dragStartInfo = JSON.parse(data) as DragStartInfo;
-        this.props.containerDrop(dragStartInfo, this.props.col, this.props.rowIdx);
+        props.containerDrop(dragStartInfo, props.col, props.rowIdx);
     }
 
 
-    itemClick = (e: React.MouseEvent, fromArrIdx: number) => {
+    let itemClick = (e: React.MouseEvent, fromArrIdx: number) => {
         e.stopPropagation(); //阻止事件冒泡
 
         if (!e.ctrlKey) return;
 
 		if (!e.shiftKey) {
 			//选取
-			if (this.props.col === FromToColumn.to) return;
-			this.props.setItemSelected(fromArrIdx);
+			if (props.col === FromToColumn.to) return;
+			props.setItemSelected(fromArrIdx);
 		} else {
             //释放
-			this.containerClick(e);
+			containerClick(e);
 		}
     }
 
-    render() {
-        return (<div className="To-item" onDrop={this.containerDrop} onClick={this.containerClick} draggable="false" onDragOver={this.allowDrop}>
-            {
-                this.props.items.map((item) => 
-                    <ItemForDrag 
-                        key={this.props.col + this.props.rowIdx} 
-                        itemInfo={item} 
-                        dragStart={this.dragStart} 
-                        itemClick={this.itemClick}
-                    />)
-            }
-        </div>);
-    }
+    return (<div className="To-item" onDrop={containerDrop} onClick={containerClick} draggable="false" onDragOver={(e)=>e.preventDefault()}>
+        {
+            props.items.map((item) => 
+                <ItemForDrag 
+                    key={props.col + props.rowIdx} 
+                    itemInfo={item} 
+                    dragStart={dragStart} 
+                    itemClick={itemClick}
+                />)
+        }
+    </div>);
 }
     
+export default ItemContaner;

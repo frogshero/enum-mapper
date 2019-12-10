daaf21d5a12868a922690fa398c56943829f0ded
hubery <guofu.wang@dianrong.com>
2019-12-10 15:08:18 +0800

    test

--- src/ItemForDrag.tsx
+++ src/ItemForDrag.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import {DragItemParam} from './EnumInfo'
 
-export default class ItemForMapping extends React.Component<DragItemParam> {
-    allowDrop = (ev: React.DragEvent) => {
-		ev.preventDefault();
-    }
-
-    render() {
-        let itemInfo = this.props.itemInfo;
+const ItemForMapping = (props: DragItemParam) => {
+        let itemInfo = props.itemInfo;
         return <div className={itemInfo.selected ? "From-item From-item-selected" : "From-item"} 
-            onClick={(e)=>this.props.itemClick(e, itemInfo.idx)} 
-            draggable="true" 
-            onDragStart={(e)=>this.props.dragStart(e, itemInfo.idx)}>
-            {itemInfo.display}</div>;
-    }
+              onClick={(e)=> props.itemClick(e, itemInfo.idx)} 
+              draggable="true" 
+              onDragStart={(e)=>props.dragStart(e, itemInfo.idx)}>
+              {itemInfo.display}
+            </div>;
 }
+
+export default ItemForMapping;
